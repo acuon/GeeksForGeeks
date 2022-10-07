602a61a4ea71a4c97a6dc38c526cfe33573632f4
Rohit Sharma <83219534+acuon@users.noreply.github.com>
2022-10-07 21:47:05 +0530

    Create FaultyWiringAndBulbs.java

---
+++ Problems/Faulty wiring and Bulbs/FaultyWiringAndBulbs.java
@@ -0,0 +1,31 @@
+package main.GFG.greedy;
+
+public class FaultyWiringAndBulbs {
+    public static void main(String[] args) {
+        int[] arr = new int[]{1, 0, 0, 1, 0, 1, 0, 1};
+        int n = arr.length;
+        System.out.println(flipCount(arr, n));
+        arr = new int[]{0, 0, 0, 0};
+        System.out.println(flipCount(arr, arr.length));
+        arr = new int[]{1, 0, 0, 1};
+        System.out.println(flipCount(arr, arr.length));
+    }
+    public static int flipCount(int[] arr, int n) {
+        int count = 0;
+        for(int i=0; i<n; i++) {
+            if((arr[i] + count) % 2 == 0) {
+                count++;
+            }
+        }
+        return count;
+    }
+}
+/*
+    1 0 0 1 0 1 0 1
+    1 1 1 0 1 0 1 0
+    1 1 1 1 0 1 0 1
+    1 1 1 1 1 0 1 0
+    1 1 1 1 1 1 0 1
+    1 1 1 1 1 1 1 0
+    1 1 1 1 1 1 1 1
+ */
